15e4e45f4786180f3cd97e3937d6b6692b11864b
alerin <lasttrinex@gmail.com>
2026-05-17 19:21:47 +1000

    add new elems

---
+++ book/l7-example_use_numpy.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.array([0,1,2,3])
+b = np.array([4,5,6,7])
+c = np.array([
+    [0,1,2,3],
+    [4,5,6,7]
+])
+d = np.zeros((2,4))
+e = np.random.rand(2,5)
+
+
+rand = np.random.rand(16, 12)
+
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+print(rand)
